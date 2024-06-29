2f790c94211b8a215cb3bb32c2d5d61b2c2cba10
Maurizio Giordano <maurizio.giordano@cnr.it>
2024-06-29 16:15:25 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
    version='1.0',
    author='Maurizio Giordano and Ilaria Granata',
    author_email='maurizio.giordano@cnr.it',
-   packages=['icarlearn', 'icarlearn.ensemble', 'icarlearn.validate'],
+   packages=['icarlearn', 'icarlearn.ensemble', 'icarlearn.validation'],
    license='LICENSE.txt',
    description='Voting Ensembling Classifiers with Dataset Splitting to Address Unbalancing',
    long_description=open('README.md').read(),
