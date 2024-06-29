9c7a1802c40f3425fa5ab11ae258ed0b76101349
Maurizio Giordano <maurizio.giordano@cnr.it>
2024-06-29 23:12:08 +0200

    upd

--- icarlearn/validation/crossvalidate.py
+++ icarlearn/validation/crossvalidate.py
@@ -19,32 +19,39 @@ else:
 import numpy as np
 import random
 
-def evaluate_fold(train_x, train_y, test_x, test_y, estimator, genes, test_genes, targets, predictions, probabilities, fold, nclasses=2):
+pred_metrics = [roc_auc_score, top_k_accuracy_score, average_precision_score]
+scorer =    {'Accuracy'  : (accuracy_score , {}),
+             'BA'  :       (balanced_accuracy_score , {}),
+             'Precision' : (precision_score, {'pos_label': 1, 'average':'macro'}),
+             'Recall'    : (recall_score   , {'pos_label': 1, 'average':'macro'}),
+             'MCC' :       (matthews_corrcoef, {}), 
+            }
+
+def evaluate_fold(train_x, train_y, test_x, test_y, estimator, indices, test_indices, targets, 
+                  predictions, probabilities, fold, scorer=scorer):
     # Initialize classifier
     clf = clone(estimator)
     clf.fit(train_x, train_y)
     probs = clf.predict_proba(test_x)
     preds = clf.predict(test_x)
     #preds = clf.classes_[np.argmax(probs, axis=1)]
-    genes = np.concatenate((genes, test_genes))
+    indices = np.concatenate((indices, test_indices))
     targets = np.concatenate((targets, test_y))
     cm = confusion_matrix(test_y, preds)
     predictions = np.concatenate((predictions, preds))
     probabilities = np.concatenate((probabilities, probs[:, 0]))
 
     # Calculate and store evaluation metrics for each fold
-    roc_auc = roc_auc_score(test_y, probs[:, 1]) if nclasses == 2 else roc_auc_score(test_y, probs, multi_class="ovr", average="macro")
-    metrics = {"index": fold,
-               "ROC-AUC" : roc_auc, 
-               "Accuracy" : accuracy_score(test_y, preds),
-               "BA" : balanced_accuracy_score(test_y, preds), 
-               "Sensitivity" : cm[1, 1] / (cm[1, 0] + cm[1, 1]),
-               "Specificity" : cm[0, 0] / (cm[0, 0] + cm[0, 1]), 
-               "MCC" : matthews_corrcoef(test_y, preds), 
-               'CM' : cm}
-    return genes, targets, predictions, probabilities, metrics
+    metrics = {"index": fold}
+    for key in scorer.keys():
+        metric, kwargs = scorer[key]
+        if metric in pred_metrics:
+            metrics[key] = metric(test_y, probs[:, 1], **kwargs) if len(clf.classes_) == 2 else metric(test_y, probs, **kwargs)
+        else:
+            metrics[key] = metric(test_y, preds, **kwargs)
+    return indices, targets, predictions, probabilities, metrics
     
-def skfold_cv(X, Y, estimator, n_splits=10, verbose: bool = False, show_progress: bool = False, seed: int = 42):
+def skfold_cv(X, Y, estimator, n_splits=10, scorer=scorer, verbose: bool = False, show_progress: bool = False, seed: int = 42, precision:int=3):
     """
     Perform cross-validated predictions using a classifier.
 
@@ -74,10 +81,15 @@ def skfold_cv(X, Y, estimator, n_splits=10, verbose: bool = False, show_progress
     # check estimator
     assert is_classifier(estimator) and hasattr(estimator, 'fit') and callable(estimator.fit) and hasattr(estimator, 'predict_proba') and callable(estimator.predict_proba), "Bad estimator imput!"
 
-    # get list of genes
-    allgenes = Y.index
-    X = X.values
-    y = Y.values.ravel()
+    # get list of indices
+    if isinstance(X, pd.DataFrame):
+        X = X.values
+    if isinstance(Y, pd.DataFrame):
+        all_indices = Y.index
+        y = Y.values.ravel()
+    else:
+        y = Y
+        all_indices = np.arange(0,len(y))
 
     # Set random seed
     random.seed(seed)
@@ -87,7 +99,7 @@ def skfold_cv(X, Y, estimator, n_splits=10, verbose: bool = False, show_progress
     kf = StratifiedKFold(n_splits=n_splits, shuffle=True, random_state=seed)
 
     nclasses = len(np.unique(y))
-    genes = np.array([], dtype=str)
+    indices = np.array([], dtype=str)
     targets = np.array([], dtype=np.int64)
     predictions = np.array([], dtype=np.int64)
     probabilities = np.array([], dtype=np.int64)
@@ -100,23 +112,23 @@ def skfold_cv(X, Y, estimator, n_splits=10, verbose: bool = False, show_progress
     for fold, (train_idx, test_idx) in enumerate(tqdm(kf.split(np.arange(len(X)), y), total=kf.get_n_splits(), 
                                                    desc=f"{n_splits}-fold", disable=not show_progress)):
         X_train, y_train = X[train_idx], y[train_idx]
-        genes, targets, predictions, probabilities, metrics = evaluate_fold(X_train, y_train, X[test_idx], y[test_idx], 
-                                                                            estimator, genes, allgenes[test_idx], targets, 
-                                                                            predictions, probabilities, fold, nclasses)
+        indices, targets, predictions, probabilities, metrics = evaluate_fold(X_train, y_train, X[test_idx], y[test_idx], 
+                                                                            estimator, indices, all_indices[test_idx], targets, 
+                                                                            predictions, probabilities, fold, scorer)
         scores = pd.concat([scores, pd.DataFrame.from_dict(metrics, orient='index').T.set_index('index')], axis=0)
 
     # Calculate mean and standard deviation of evaluation metrics
-    df_scores = pd.DataFrame([f'{val:.4f}±{err:.4f}' for val, err in zip(scores.loc[:, scores.columns != "CM"].mean(axis=0).values,
-                                                                     scores.loc[:, scores.columns != "CM"].std(axis=0))] +
-                             [(scores[['CM']].sum()).values[0].tolist()],
+    fmt = '{:.%df}' % precision
+    df_scores = pd.DataFrame([f"{fmt.format(val)}±{fmt.format(err)}" for val, err in zip(scores.mean(axis=0).values,
+                                                                     scores.std(axis=0))],
                              columns=['measure'], index=scores.columns)
 
     # Create DataFrame for storing detailed predictions
-    df_results = pd.DataFrame({'gene': genes, 'label': targets, 'prediction': predictions, 'probabilities': probabilities}).set_index(['gene'])
+    df_predictions = pd.DataFrame({'indices': indices, 'label': targets, 'prediction': predictions, 'probabilities': probabilities}).set_index(['indices'])
 
     # Save detailed predictions to a CSV file if requested
     if in_notebook():
-        ConfusionMatrixDisplay(confusion_matrix=np.array(df_scores.loc['CM']['measure']), display_labels=np.unique(y)).plot()
+        ConfusionMatrixDisplay(confusion_matrix=confusion_matrix(df_predictions['label'].values,df_predictions['prediction'].values), display_labels=np.unique(y)).plot()
 
     # Return the summary statistics of cross-validated predictions, the single measures and the prediction results
-    return df_scores, scores, df_results
+    return df_scores, scores, df_predictions
