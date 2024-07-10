c6652d4b27006045b9b0bd4c046241ead10506d3
Maurizio Giordano <maurizio.giordano@cnr.it>
2024-07-10 15:36:39 +0200

    upd

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-   name='ICARlearn',
+   name='SVElearn',
    version='1.0',
    author='Maurizio Giordano and Ilaria Granata',
    author_email='maurizio.giordano@cnr.it',
-   packages=['icarlearn', 'icarlearn.ensemble', 'icarlearn.validation'],
+   packages=['svelearn', 'svelearn.ensemble', 'svelearn.validation'],
    license='LICENSE.txt',
    description='Voting Ensembling Classifiers with Dataset Splitting to Address Unbalancing',
    long_description=open('README.md').read(),
