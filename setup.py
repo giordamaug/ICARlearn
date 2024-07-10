c565ef67fc76463372d043cc20e8ec9afa1439e3
Maurizio Giordano <maurizio.giordano@cnr.it>
2024-07-10 16:16:17 +0200

    upd

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
    version='1.0',
    author='Maurizio Giordano and Ilaria Granata',
    author_email='maurizio.giordano@cnr.it',
-   packages=['svelearn', 'svelearn.ensemble', 'svelearn.validation'],
+   packages=['svelearn', 'svelearn.models', 'svelearn.validation'],
    license='LICENSE.txt',
    description='Voting Ensembling Classifiers with Dataset Splitting to Address Unbalancing',
    long_description=open('README.md').read(),
